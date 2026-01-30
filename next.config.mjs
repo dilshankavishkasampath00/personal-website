5941cd709c894d5d5e340199d337d97ec49ea824
dilshankavishkasampath00 <your-email@example.com>
2026-01-30 12:58:28 +0530

    fix: remove basePath from production config to fix CSS styling on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? `/venus-nextjs` : "";
-
 const nextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
     qualities: [75, 100],
   },
   trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+  experimental: {
+    optimizePackageImports: ['@iconify/react'],
   },
 };
 
